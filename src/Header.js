b4aa2202e8889bd2c1df6395098ffadc9289a3a4
Rafael Alfaro <rafalfaro_18@outlook.com>
2018-11-15 17:15:35 -0600

    Adds overlay to header

--- src/Header.js
+++ src/Header.js
@@ -15,16 +15,22 @@ const hrStyle = {
   marginRight: `${hrPaddingX}%`,
 };
 
+const overlayStyle = {
+  backgroundColor : '#00000096'
+};
+
 class Header extends Component {
     render() {
       return (
-        <div className="container-fluid h-100 d-flex rar-header-component" style={divStyle}>
-          <div className="jumbotron jumbotron-fluid bg-transparent my-auto w-100">
-            <h1 className="display-4 text-white text-center">
-              Rafael Alfaro
-              <hr style={hrStyle}/>
-              Resume
-            </h1>
+        <div className="container-fluid p-0 h-100 rar-header-component" style={divStyle}>
+          <div className="rar-bg-overlay h-100 d-flex" style={overlayStyle}>
+            <div className="jumbotron jumbotron-fluid bg-transparent my-auto w-100">
+              <h1 className="display-4 text-white text-center">
+                Rafael Alfaro
+                <hr style={hrStyle}/>
+                Resume
+              </h1>
+            </div>
           </div>
         </div>
       );
