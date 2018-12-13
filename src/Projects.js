cd8011c9a796de3af69a88ee97794ebcc2ecfa04
Rafael Alfaro <rafalfaro_18@outlook.com>
2018-12-12 20:31:25 -0600

    Adds projects section title and hr

--- src/Projects.js
+++ src/Projects.js
@@ -7,12 +7,22 @@ const divStyle = {
     backgroundColor: section_color
 };
 
+const hrStyle = {
+    borderColor: '#fff'
+};
+
 class Projects extends Component {
     render(){
         return (
         <div className="container-fluid rar-projects-component" style={divStyle}>
-            <p>This is the projects section</p>
-            <br/>
+            <div className="text-center row">
+                <h2 className="w-100">Projects</h2>
+            </div>
+            <div className="row">
+                <div className="col-8 offset-2">
+                    <hr style={hrStyle} className="w-100"/>
+                </div>
+            </div>
         </div>
         );
     }
