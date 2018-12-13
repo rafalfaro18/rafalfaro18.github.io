2183972f822fcc98c4a24ed685f4c20f9af064d0
Rafael Alfaro <rafalfaro_18@outlook.com>
2018-12-12 22:08:18 -0600

    Adds download resume button

--- src/Profile.js
+++ src/Profile.js
@@ -27,6 +27,11 @@ class Profile extends Component {
               <h4 className="w-100">About Me</h4>
               <p className="w-100">I’m a web systems engineer and WordPress Enthusiast. I’m Costa Rican but I’m also fluent in english. I’m not the typical geek, i’m outgoing and very social. However I do love video games but also traveling, craft beer and all sorts of food. I like beeing part of interesting and challenging projects that bring out the best of me and my abilities as an engineer.</p>
             </div>
+          </div>
+          <div className="row">
+            <div className="text-center mx-auto mb-4">
+              <a className="btn btn-info" rel="noopener noreferrer" target="_blank" href="https://docs.google.com/document/d/1nmJ-xjhzjXZ5o7FGsLGMZmVV-dihPPN63EVRm9uvciI/export?format=pdf">Download Resume</a>
+            </div>
           </div>  
         </div>
       );
