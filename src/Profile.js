7194592792ce86c20d2950825a483f3c45be49e8
Rafael Alfaro <rafalfaro_18@outlook.com>
2018-12-12 21:42:09 -0600

    adds profile text

--- src/Profile.js
+++ src/Profile.js
@@ -16,7 +16,16 @@ class Profile extends Component {
           </div>
           <div className="row">
             <div className="col-8 offset-2">
-                <hr style={hrStyle} className="w-100"/>
+              <hr style={hrStyle} className="w-100"/>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-3 offset-md-2">
+              <img alt="" className="img-fluid rounded-circle" src={profileImage} />
+            </div>
+            <div className="text-center col-md-5">
+              <h4 className="w-100">About Me</h4>
+              <p className="w-100">I’m a web systems engineer and WordPress Enthusiast. I’m Costa Rican but I’m also fluent in english. I’m not the typical geek, i’m outgoing and very social. However I do love video games but also traveling, craft beer and all sorts of food. I like beeing part of interesting and challenging projects that bring out the best of me and my abilities as an engineer.</p>
             </div>
           </div>  
         </div>
