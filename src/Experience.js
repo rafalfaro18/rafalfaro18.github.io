0e3907ccbb46926f558a16c372ca895c1745f9ad
Rafael Alfaro <rafalfaro_18@outlook.com>
2018-12-13 21:27:33 -0600

    Adds location to first experience

--- src/Experience.js
+++ src/Experience.js
@@ -42,6 +42,7 @@ class Experience extends Component {
                 </div>
                 <div className="col-md-5 offset-md-1">
                     <p>Education was web development focused with PHP, HTML, CSS, Javascipt, Ajax, Apache, MySQL, jQuery. During this time in university I specialized in web development. Here I did my thesis project: 'PIEUNA' a Joomla based social network for Universidad Nacional alumni. I also learned about Project Management and Research Design.</p>
+                    <p><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z"/></svg> Costa Rica</p>
                 </div>
             </div>
         </div>
