78849e49ab23c30912ce1064eddc4e71dad31323
Rafael Alfaro <rafalfaro_18@outlook.com>
2018-12-13 21:24:37 -0600

    Adds one experience

--- src/Experience.js
+++ src/Experience.js
@@ -23,8 +23,26 @@ class Experience extends Component {
                     <hr style={hrStyle} className="w-100"/>
                 </div>
             </div>
-            <div className="text-center row">
-                <p className="w-100">Coming Soon... See on <a href="https://www.linkedin.com/in/rafalfaro18">linkedin</a></p>
+            <div className="row">
+                <div className="col-md-2 offset-md-2">
+                    <h4>Education</h4>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-2 offset-md-2">
+                    <strong>Universidad Nacional</strong>
+                </div>
+                <div className="col-md-5 offset-md-1">
+                    <strong>Licentiate - Computer Science (Specialization: Web Systems)</strong>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-2 offset-md-2">
+                    <p>Feb 2015 - Jun 2016</p>
+                </div>
+                <div className="col-md-5 offset-md-1">
+                    <p>Education was web development focused with PHP, HTML, CSS, Javascipt, Ajax, Apache, MySQL, jQuery. During this time in university I specialized in web development. Here I did my thesis project: 'PIEUNA' a Joomla based social network for Universidad Nacional alumni. I also learned about Project Management and Research Design.</p>
+                </div>
             </div>
         </div>
         );
