2525864432a5de4b343418b3a55bd7ae09394f5a
Rafael Alfaro <rafalfaro_18@outlook.com>
2018-12-13 21:33:26 -0600

    Adds third experience

--- src/Experience.js
+++ src/Experience.js
@@ -62,6 +62,23 @@ class Experience extends Component {
                     <p><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z"/></svg> Costa Rica</p>
                 </div>
             </div>
+            <div className="row">
+                <div className="col-md-2 offset-md-2">
+                    <strong>Universidad Nacional</strong>
+                </div>
+                <div className="col-md-5 offset-md-1">
+                    <strong>Diploma - Computer Science</strong>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-2 offset-md-2">
+                    <p>Feb 2010 - Nov 2013</p>
+                </div>
+                <div className="col-md-5 offset-md-1">
+                    <p>Education was mainly C++ based programming but I also learned about HTML, CSS, PHP, MVC, UML, Java, Networking, Database Administration, Linux, Tech Support, Object Oriented Programming, English, Calculus, Linear Algebra, Probability and Statistics, Discrete Mathematics and more.</p>
+                    <p><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z"/></svg> Costa Rica</p>
+                </div>
+            </div>
         </div>
         );
     }
