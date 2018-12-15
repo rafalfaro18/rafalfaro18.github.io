4890bbcd56043fbb159d8894ef7f44b57a2327bd
Rafael Alfaro <rafalfaro_18@outlook.com>
2018-12-15 01:01:30 -0600

    Fixes console errors

--- src/Rating.js
+++ src/Rating.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
 const ratingStyle = {
-    display: "inline-block",
     float: "right" 
 };
 
@@ -9,11 +8,11 @@ class Rating extends Component {
     render() {
         var stars = [];
         for( let i = 0; i < this.props.count; i++ ) {
-            stars.push(<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 .288l2.833 8.718h9.167l-7.417 5.389 2.833 8.718-7.416-5.388-7.417 5.388 2.833-8.718-7.416-5.389h9.167z"/></svg>);
+            stars.push(<svg key={i} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 .288l2.833 8.718h9.167l-7.417 5.389 2.833 8.718-7.416-5.388-7.417 5.388 2.833-8.718-7.416-5.389h9.167z"/></svg>);
         }
 
       return (
-        <div style={ratingStyle} className="rating">{stars}</div>
+        <span style={ratingStyle} className="rating">{stars}</span>
       );
     }
   }
