1ea11f9e87db242c62255cd17b207af7ce38cadf
Rafael Alfaro <rafalfaro_18@outlook.com>
2018-12-16 00:50:40 -0600

    Adds spanish translation

--- src/i18n.js
+++ src/i18n.js
@@ -2,11 +2,15 @@ import i18n from "i18next";
 import { reactI18nextModule } from "react-i18next";
 
 import translationEN from './locales/en/translation.json';
+import translationES from './locales/es/translation.json';
 
 // the translations
 const resources = {
   en: {
     translation: translationEN
+  },
+  es: {
+    translation: translationES
   }
 };
 
