bfbcf86a4b34e342413751f768694019cadf002c
Rafael Alfaro <rafalfaro_18@outlook.com>
2018-12-18 20:38:25 -0600

    translates resume title

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { withNamespaces } from 'react-i18next';
 
 const imgMyimageexample = require('./assets/img/ilya-pavlov-87438.jpg');
 const divStyle = {
@@ -19,22 +20,20 @@ const overlayStyle = {
   backgroundColor : '#00000096'
 };
 
-class Header extends Component {
-    render() {
-      return (
-        <div className="container-fluid p-0 h-100 rar-header-component" style={divStyle}>
-          <div className="rar-bg-overlay h-100 d-flex" style={overlayStyle}>
-            <div className="jumbotron jumbotron-fluid bg-transparent my-auto w-100">
-              <h1 className="display-4 text-white text-center">
-                Rafael Alfaro
-                <hr style={hrStyle}/>
-                Resume
-              </h1>
-            </div>
+function Header({ t }) {
+    return (
+      <div className="container-fluid p-0 h-100 rar-header-component" style={divStyle}>
+        <div className="rar-bg-overlay h-100 d-flex" style={overlayStyle}>
+          <div className="jumbotron jumbotron-fluid bg-transparent my-auto w-100">
+            <h1 className="display-4 text-white text-center">
+              Rafael Alfaro
+              <hr style={hrStyle}/>
+              {t('Resume')}
+            </h1>
           </div>
         </div>
-      );
-    }
+      </div>
+    );
   }
   
-  export default Header;
+  export default withNamespaces()(Header);
