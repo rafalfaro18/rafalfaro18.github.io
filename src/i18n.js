1e1daac35d69dacda8cbb268d1c5d32037b17997
Rafael Alfaro <rafalfaro_18@outlook.com>
2018-12-18 20:52:44 -0600

    Enables translation definition with sub keys

--- src/i18n.js
+++ src/i18n.js
@@ -20,8 +20,6 @@ i18n
     resources,
     lng: "en",
 
-    keySeparator: false, // we do not use keys in form messages.welcome
-
     interpolation: {
       escapeValue: false // react already safes from xss
     }
