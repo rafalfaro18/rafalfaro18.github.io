faab6a7e8787a51c219804ba546bcf9bd5dbf705
Rafael Alfaro <rafalfaro_18@outlook.com>
2023-01-02 11:04:20 -0600

    Updates

---
+++ precache-manifest.5539cd339d0d42e6ec8c5a7093c602e9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "e815520d7a981dbef8ca",
+    "url": "/static/css/main.eb678f70.chunk.css"
+  },
+  {
+    "revision": "e815520d7a981dbef8ca",
+    "url": "/static/js/main.e815520d.chunk.js"
+  },
+  {
+    "revision": "f03ab837485a4c636588",
+    "url": "/static/js/1.f03ab837.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f1eb6a65f7f5bd5e05cd461f0fae8d35",
+    "url": "/static/media/ilya-pavlov-87438.f1eb6a65.jpg"
+  },
+  {
+    "revision": "c7351dd7227eaa3f64e4e7fde144d5d5",
+    "url": "/static/media/rafael_alfaro.c7351dd7.jpg"
+  },
+  {
+    "revision": "6e62bef836458e644bea6ea02940cb78",
+    "url": "/static/media/marriott-hotels.marriott.com_marriott-hotels-ted.6e62bef8.jpg"
+  },
+  {
+    "revision": "867c1988fdc5613bce52d4a64b795b9e",
+    "url": "/static/media/www.enchanting-costarica.com.867c1988.jpg"
+  },
+  {
+    "revision": "f37e11454ff6584f80b3d82f1a2f259b",
+    "url": "/static/media/www.stayincostarica.com.f37e1145.jpg"
+  },
+  {
+    "revision": "5f4c49430d7765c6a666b2dc01c6908a",
+    "url": "/static/media/www.tabacon.com.5f4c4943.jpg"
+  },
+  {
+    "revision": "bb6e6c818340c3027f4b4cc92d79ef04",
+    "url": "/index.html"
+  }
+];
